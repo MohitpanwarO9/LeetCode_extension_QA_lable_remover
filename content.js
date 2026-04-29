505beda82640e84b8d1c7b292fa834a104ab014c
Mohitpanwar09 <mohitpanwar9420@gmail.com>
2026-04-30 01:22:36 +0530

    1.1_working_basic_2_page_

--- content.js
+++ content.js
@@ -1,6 +1,32 @@
 console.log("Extension is running...");
-const lable = document.querySelector(".text-difficulty-hard")
 
-if(lable){
-    lable.remove();
+const url = window.location;
+const hostname = url.hostname;
+let path = url.pathname;
+let pathName = path.split("/")
+
+console.log(url)
+console.log(hostname, "\n",pathName)
+
+const pageName = pathName[1];
+console.log(pageName);
+
+if(pageName === "problemset"){
+    console.log("Hello heloo")
+    setTimeout(() => {
+        const POTD_lable = document.querySelector('a[href*="/problems/"] p.text-sd-hard');
+        console.log(POTD_lable);
+        if(POTD_lable){
+            POTD_lable.remove()
+        }
+        // console.log(p);
+    }, 5000);
+}
+
+if(pageName === "problems"){
+    const lable = document.querySelector(".text-difficulty-hard, .text-difficulty-easy, .text-difficulty-medium")
+    
+    if(lable){
+        lable.remove();
+    }
 }
